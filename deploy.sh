68eb00681a0a7fd78944223994e52f33ecf894cb
Kolya142 <nikolasshevelkos@gmail.com>
2026-01-04 18:29:47 +0300

    Some useful things

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -rvf * .gitignore ~/Programming/Kolya142.github.io/wheregp
+cd ~/Programming/Kolya142.github.io/wheregp
+bash
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const script_s_whereGP_version = 5;
+const script_s_whereGP_version = 6;
+
+const kolya142s_key = "-----BEGIN PGP PUBLIC KEY BLOCK-----\n\nxsFNBGkJyGgBEACtLn/DpUmujh77NbESzVa3/Mt8p/uyaYL3prUP2+3rbdakFYH1\n7AnDIcbdXPAuwQFU7Rm+GkojwfhBSiXNxZKPgSoU5h/dAdia5z/hCYbtkBN0hRlI\nQILKDPl58DkVBnkvxQj7T1vYfGagIilCHTe36WUW8Z7+iPRUwBnMF6MsISNNlfEN\ndY9ZPhraQleCcqP+gmTcJrgZKrb0ksdfqKja8Scct3G0fQjl5jVB7YR3xxRoyLmx\ngHmHnaQYf2/APHAfMylkjfm15c5IA9XqRbhbBIvLvu1H9tkH5vDAQ/rop8j6WycP\nWkePp2FlxChct3vtHVMO/19ARSz2EGp+talFYm8GJET47unZUNq4T+VYLic4GeE3\n4N304mxFi++blCl98MwQuDGVcCA7ZXkvniG0mc6sJWrkONDHWUAp+gC6FoLq5ng2\neWyqbAc5NqNe7WxPo5va6NpYCJ9+eEcGjiCpisCVAPqo0OJIibw0GGL3ytzKQV90\nGBWTmnoOBM8ODtpFiUoemPpQwxuNLQZPdmj/S9ZKGWo++FGzqVFnFy2pe1ulOOtt\nWky91DxcQfaeQIJ+SUYb2C4skoKFHQyC9ho0BYt8nAne07UqthBkna7e1Ba5LTW7\n3Lzh+jnuh+73lc4xPHn0A2umHlCg5+gRjO0QdXEFCkM595CNIPH0Tmsd8QARAQAB\nzTlOaWtvbGF5IFNoZXZlbGtvIChHbnVQRyBrZXkpIDxuaWtvbGFzc2hldmVsa29z\nQGdtYWlsLmNvbT7CwY4EEwEKADgWIQRuNE9qFb0tzDVM1AQrt0D7bUrlIwUCaQnI\naAIbAwULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAAKCRArt0D7bUrlI5eHD/9j/lS7\naTMfMtsK4/nAwP/X21edW9sJvzyiDmFH9HMShUtuktgQol4soYxeh4/megi/jAAv\npN2klTeVO5pjuDj6jfpHyPre0n2hQrQH41Jo8mTyKDASJbN94grkiQpcn4LYpxeD\n5NdmHWQeyDrj+XXGD689fiqtGWeQJWut+p9dhzapFaPdXBysiJp3ejXI8gLJOCJy\n2axn5LVnVhoVv9n7GkAffPreikNxm4W1DlvYlBp+D+gP3e0rw0Hf0deN2P2svq2j\nkfz8/QNsu9Z9ydiW64hK6+ZVqL+3zsfS8NBJuAlLw3Ys8yV5kZv4KiqlMMM2a/af\nGe0oyn27lrJVzfXWQDkHJUyzKSr4CDj8cvkKwfyL86Sv34emMiGQZfoje3deENNE\nC9/YgKJO6oKnSwXYt/WOq5IaP0tyV3od/anbtEWrrXsEATdTwJqswmHlixo9Hk2h\nTuNCeasp+0NXmDt/Exuo6mOQSFEzTox7Gq3qpkv0ubNQBFEjTf9252qqu23c+0Ge\nZ61ed8YmgWjHscp/WIHcNZjtC3YIEO3KKBMZyFwqfHza94etFcKDYFOTYcJWEYra\ncqYM3btVGXSRWvBEGwp8n/yrddJVwZLyK/6GIuYxrDLhIjom/FZKaMro7xpi/jK1\nAuiY2QvszYGBUz8KdV2sebGuGLTGOrHKhTe+c87BTQRpCchoARAA4tRqap+PDY+e\nu7UzR4FOdu7TYCLlYVqP1B/z48Q4ROlzlXYsZgtOS/YRUqwqsATaXs0Ty7WYVlFT\nK9egPs2oAWBaJBRpLwnzKaO8I+7hu4PQZD7+Dz5SCs/6A3FsWCdOq+qms1jxyWDp\nGvs+TAl0TQLgwh5sj575DX01bHUNJCUgjSzp+cN/A3cb6C8+2flx+CgtRu1Kc4FF\n2IqphcH5TxTmvTXeIcELo36XO2uwn83KdP6Qi0ZDrkDZYsdeTE5+f0vd0Gv1VgHh\ndjz57dNgpUAaEt49Z/vT3r9Q+jQ5p/JM2V89CB5PzWXEhGt58YKeTeB0PJVZYCCB\nZf0vkiIJF14n5dFOS/OHKVUbI+UkcGocwuY6Ir91wV61x01eDHzxH5CQyzxrCfHL\nEycrDfP5BEIybxwQlEuGe1SewMs3oIMniwg5cC53GO/zBSemNf8u0m/04MvmRh3L\nnn3NY7mGm9lrHVTL9SOj9v5CHDchEmYyzi8X3SuRe7NGN7g6ZhrjWOn/yZ/BPSGc\n3Tij5GGjVwqDmIeXl78fkgJg/ArD26YCMaBlCGxlh1GWSchp8Xd6LQWD6ifVVdp5\nldAnUIkYPRO9iTyPCuAAiMrmgnK98QGvJ1ITcCBXzZ1zrKho/Y193EQQ0wK7tXab\nU486+K5PtfrpOGg7tm9IkGGYpnxRwA8AEQEAAcLBdgQYAQoAIBYhBG40T2oVvS3M\nNUzUBCu3QPttSuUjBQJpCchoAhsMAAoJECu3QPttSuUj7SYQAITVWSxQcEj3FYbO\n5dnraBX2BsDLWcJYu6aVDcqPJKZY+gqPMYrr07ifnn8twttSM2hJ52YmtgOVGlrL\nq4GXlis+ndZRzHBI7Ss+cgj33EmMliOPH5MCXp7cAGgP7znucP8ITkiTIBdcX7YL\nHvhF1mAfsYs/dfkMhVyrg+HKIWP87OjpKP8s8/l7BUaWHsZsVXojH3LzABDewj0/\n6kAcNKDUc0bOuQlJVtbh7IUXn/3FmCtYTTY4lX5Cau4D4seTGu+Bj7teRPeDEWMz\nWoaTFqqcfCKlhLPBftnsDF9Xxp60WpSbLvQUS/eJcJFw2fqkVoW4xfrhPY/TuMIK\nQQ9z5E1Uyc98AKmwjEQOa6CkuS3oJ/D1Ff9eW9CDZiacHklJ7o7PbZU9N0d095BS\nC0biqJQpQKtkxUT06yl1Uoi0H9YLubo8IQOyhXo1ANYWxWnwdaI/PS40dgTqBZNh\njaXwXU73JX/qPb2T0j/WA5EZG9aULlfy9QkKqO6vRFqUv9VTAYkULiFKX1LXJrrI\nV9bt0Kukny/DIcOHhxrf27pPwtMqYQnSssMDv8aocoBApFGcazYOx4YmEByD6WIb\n/fPfvv2GBn+i8+NoMI8Fz1y2TnowY6PrdJ0Neb1OLM7IrnX4sDhV9zEjIhCvsqk9\nyJrylrxC4L1b6QXsYsRv4OTTAP+B\n=o+nY\n-----END PGP PUBLIC KEY BLOCK-----\n";
 
 if (localStorage.getItem("WGPversion") === null) {
     if (localStorage.getItem("mypriv") !== null)
@@ -13,6 +15,7 @@ console.log(user_s_whereGP_version);
 
 // 4 - before version counting.
 // 5 - added version counting. added "Owner" contact.
+// 6 - added "Kolya142" contact.
 
 if (!window.localization) {
     alert("There is no localization!!1!");
@@ -112,6 +115,8 @@ async function gtk() {
 
 if (localStorage.getItem("mypriv") === null || localStorage.getItem("mypub") === null) gk();
 
+if (localStorage.getItem("contact") === null) localStorage.setItem("contact", JSON.stringify({"Kolya142": kolya142s_key}));
+
 if (user_s_whereGP_version < 5) {
     let contact_arr = JSON.parse(localStorage.getItem("contact"));
     contact_arr["Owner"] = localStorage.getItem("mypub"); // Some people may know two languages so i choosed English.
@@ -119,69 +124,76 @@ if (user_s_whereGP_version < 5) {
     update_contacts_list();
 }
 
-if (localStorage.getItem("contact") === null) localStorage.setItem("contact", "{}");
+if (user_s_whereGP_version < 6) {
+    let contact_arr = JSON.parse(localStorage.getItem("contact"));
+    contact_arr["Kolya142"] = kolya142s_key;
+    localStorage.setItem("contact", JSON.stringify(contact_arr));
+    update_contacts_list();
+}
 
 async function update_contacts_list() {
     contacts_list.innerHTML = "";
     let is_any = false;
     const contacts = JSON.parse(localStorage.getItem("contact"));
-    for (let b of Object.keys(contacts)) {
-        is_any = true;
-        {
-            const el = document.createElement('button');
-            el.className = 'button';
-            el.innerHTML = localization.del_cn_js;
-            el.onclick = () => {
-                if(confirm(localization.do_u_want_rm_cn+b+"?")) {
-                    const p = JSON.parse(localStorage.getItem("contact"));
-                    p[b] = undefined;
-                    localStorage.setItem("contact", JSON.stringify(p));
-                    update_contacts_list();
-                }
-            };
-            contacts_list.appendChild(el);
-        }
-        {
-            const el = document.createElement('button');
-            el.className = 'button';
-            el.innerHTML = localization.rn_cn_js;
-            el.onclick = () => {
-                if (confirm(localization.do_u_want_rn_cn+b+"?")) {
-                    const nn = prompt(localization.ent_new_cn_nm+b);
-                    const p = JSON.parse(localStorage.getItem("contact"));
-                    p[nn] = p[b];
-                    p[b] = undefined;
-                    localStorage.setItem("contact", JSON.stringify(p));
-                    update_contacts_list();
-                }
-            };
-            contacts_list.appendChild(el);
-        }
-        {
-            const el = document.createElement('button');
-            el.className = 'button';
-            const key = await openpgp.readKey({armoredKey: contacts[b]}); // FIXMEE: TOO SLOW
-            if (do_restrict_real_names) {
-                el.innerText = b + " ["+localization.rstd+"]";
+    await (async () => {
+        for (let b of Object.keys(contacts)) {
+            is_any = true;
+            {
+                const el = document.createElement('button');
+                el.className = 'button';
+                el.innerHTML = localization.del_cn_js;
+                el.onclick = () => {
+                    if(confirm(localization.do_u_want_rm_cn+b+"?")) {
+                        const p = JSON.parse(localStorage.getItem("contact"));
+                        p[b] = undefined;
+                        localStorage.setItem("contact", JSON.stringify(p));
+                        update_contacts_list();
+                    }
+                };
+                contacts_list.appendChild(el);
             }
-            else {
-                if (key && key.users && key.users[0] && key.users[0].userID && key.users[0].userID.userID) {
-                    el.innerText = b + " ["+key.users[0].userID.userID+"]";
+            {
+                const el = document.createElement('button');
+                el.className = 'button';
+                el.innerHTML = localization.rn_cn_js;
+                el.onclick = () => {
+                    if (confirm(localization.do_u_want_rn_cn+b+"?")) {
+                        const nn = prompt(localization.ent_new_cn_nm+b);
+                        const p = JSON.parse(localStorage.getItem("contact"));
+                        p[nn] = p[b];
+                        p[b] = undefined;
+                        localStorage.setItem("contact", JSON.stringify(p));
+                        update_contacts_list();
+                    }
+                };
+                contacts_list.appendChild(el);
+            }
+            {
+                const el = document.createElement('button');
+                el.className = 'button';
+                const key = await openpgp.readKey({armoredKey: contacts[b]});
+                if (do_restrict_real_names) {
+                    el.innerText = b + " ["+localization.rstd+"]";
                 }
                 else {
-                    el.innerText = b + " ["+localization.inv_cn_id+"]";
+                    if (key && key.users && key.users[0] && key.users[0].userID && key.users[0].userID.userID) {
+                        el.innerText = b + " ["+key.users[0].userID.userID+"]";
+                    }
+                    else {
+                        el.innerText = b + " ["+localization.inv_cn_id+"]";
+                    }
                 }
+                el.onclick = () => {
+                    contq.value = b;
+                };
+                contacts_list.appendChild(el);
             }
-            el.onclick = () => {
-                contq.value = b;
-            };
-            contacts_list.appendChild(el);
+            {
+                const el = document.createElement('br');
+                contacts_list.appendChild(el);
+            }
         }
-        {
-            const el = document.createElement('br');
-            contacts_list.appendChild(el);
-        }
-    }
+    })();
     if (!is_any) contacts_list.innerHTML = localization.no_contacts;
 }
 
